ad76eb6d3fa94141a0a17b24d85d9991bfcc7934
Stephen McCurry <stemccurry@gmail.com>
2013-10-16 22:53:44 -0700

    salesforce

--- salesforce/apex/IncentiveBuilderClass.java
+++ salesforce/apex/IncentiveBuilderClass.java
@@ -1,73 +1,210 @@
 public class IncentiveBuilderClass {
+
+    Incentive__c incentive ;
+    IncentiveRule__c rule ;
+    IncentiveMetric__c metric ;
+    public Boolean typeUpdate { get; set;} 
+    static Map<String,Schema.SObjectType> gd = Schema.getGlobalDescribe();  
     
-    String subdomain ;
-    String startDate ;
-    String endDate ;
-    String title ;
-    String tagline ;
-    String description ;
-    
-    public String getSubdomain(){
-        return subdomain;
+    public void toggleType() {
+        ApexPages.Message myMsg = new ApexPages.Message(ApexPages.Severity.ERROR,'Error: Invalid Input.');
+        if (rule.Type__c != null && rule.Type__c =='Update') {
+                typeUpdate = true ;
+        } else {
+            typeUpdate = false ;
+        }
     }
     
-    public void setSubdomain(String nd){
-        this.subdomain = nd;
+    public Incentive__c getIncentive() {
+          if(incentive == null) incentive = new Incentive__c();
+          return incentive;
     }
     
-    public String getTitle(){
-        return title;
+    public IncentiveRule__c getRule() {
+          if(rule == null) rule = new IncentiveRule__c();
+          return rule;
     }
     
-    public void setTitle(String nd){
-        this.title = nd;
+    public IncentiveMetric__c getMetric() {
+          if(metric == null) metric = new IncentiveMetric__c();
+          return metric;
     }
     
-    public String getTagline(){
-        return tagline;
+    public List<IncentiveRule__c> rules {
+        get { 
+            if (rules == null) rules = new List<IncentiveRule__c>();
+            return rules;
+        }
+        set;
     }
     
-    public void setTagline(String nd){
-        this.tagline = nd;
+    public void removeRule() {
+		Id ruleId = ApexPages.currentPage().getParameters().get('ruleId');
+		
+        IncentiveRule__c r = [Select Id from IncentiveRule__c where Id = :ruleId];
+        
+        delete r ;
     }
     
-    public String getDescription(){
-        return description;
+    public List<IncentiveMetric__c> metrics {
+        get { 
+            if (metrics == null) metrics = new List<IncentiveMetric__c>();
+            return metrics;
+        }
+        set;
+    }
+	
+    public List<SelectOption> getNumericFields()   
+    {  
+        List<SelectOption> fields = new List<SelectOption>();
+        fields.add(new SelectOption('-- None --', '-- None --')); 
+      
+        Schema.SObjectType sobjType = gd.get(rule.Object__c);  
+        if (sobjType != null) {
+        Schema.DescribeSObjectResult r = sobjType.getDescribe();  
+        Map<String,Schema.SObjectField> M = r.fields.getMap(); 
+      
+        for(String fieldName : M.keyset())  
+            {
+                Schema.SObjectField field = M.get(fieldName);                                                      
+                Schema.DescribeFieldResult fieldDesc = field.getDescribe();  
+				
+				if (Schema.DisplayType.Integer == fieldDesc.getType() || Schema.DisplayType.Double == fieldDesc.getType() || Schema.DisplayType.Currency == fieldDesc.getType()) {
+	                fields.add(new SelectOption(fieldName.toLowerCase(), fieldDesc.getLabel())); 
+				}
+            }  
+        }
+      
+        fields.sort();
+        return fields;  
+    }  
+    
+    public List<SelectOption> getSobjectFields()   
+    {  
+        List<SelectOption> fields = new List<SelectOption>();
+        fields.add(new SelectOption('-- None --', '-- None --')); 
+      
+        Schema.SObjectType sobjType = gd.get(rule.Object__c);  
+        if (sobjType != null) {
+        Schema.DescribeSObjectResult r = sobjType.getDescribe();  
+        Map<String,Schema.SObjectField> M = r.fields.getMap();  
+      
+        for(String fieldName : M.keyset())  
+            {  
+                Schema.SObjectField field = M.get(fieldName);                                                      
+                Schema.DescribeFieldResult fieldDesc = field.getDescribe();  
+                fields.add(new SelectOption(fieldName.toLowerCase(), fieldDesc.getLabel())); 
+            }  
+        }
+      
+        fields.sort();
+        return fields;  
+    }      
+    
+    public List<SelectOption> getObjectNames()
+    {
+       List<Schema.SObjectType> gdo = Schema.getGlobalDescribe().Values(); 
+       List<SelectOption> options = new List<SelectOption>();
+       
+       options.add(new SelectOption('-- Select --', '-- Select --'));
+       
+       for(Schema.SObjectType f : gdo)
+       {
+           Schema.SObjectType sobjType = gd.get(f.getDescribe().getLabel());  
+           if (sobjType != null) {
+              options.add(new SelectOption(f.getDescribe().getLabel(),f.getDescribe().getLabel()));
+           }
+       }
+       options.sort();
+       return options;
+      
     }
     
-    public void setDescription(String nd){
-        this.description = nd;
+    public List<SelectOption> getTypes()
+    {
+      List<SelectOption> options = new List<SelectOption>();
+        
+       Schema.DescribeFieldResult fieldResult =
+       IncentiveRule__c.Type__c.getDescribe();
+       List<Schema.PicklistEntry> ple = fieldResult.getPicklistValues();
+        
+       for( Schema.PicklistEntry f : ple)
+       {
+          options.add(new SelectOption(f.getLabel(), f.getValue()));
+       }       
+       return options;
     }
     
-     public String getStartDate(){
-        return startDate;
+    public List<IncentiveMetric__c> getAvailableMetrics() {
+        List<IncentiveMetric__c> metrics = new List<IncentiveMetric__c>();
+        
+        for( IncentiveRule__c rule : rules)
+        {
+            if (rule.PointsField__c != null) 
+                metrics.add(buildMetric(rule.Object__c, rule.PointsField__c));
+            
+            if (rule.Field__c != null) 
+                metrics.add(buildMetric(rule.Object__c, rule.Field__c));
+        }
+        
+        return metrics;
+        
     }
     
-    public void setStartDate(String nd){
-        this.startDate = nd;
+    private IncentiveMetric__c buildMetric(String objectName, String fieldName) {
+        IncentiveMetric__c metric = new IncentiveMetric__c() ;
+        
+        metric.Object__c = objectName ;
+        metric.Field__c = fieldName ;
+        metric.IsDisplayed__c = false ;
+        metric.Target__c = 0 ;
+        
+        return metric ;
     }
     
-     public String getEndDate(){
-        return endDate;
+    public PageReference cancel() {
+      return Page.InvAdmin;
     }
     
-    public void setEndDate(String nd){
-        this.endDate = nd;
+    public PageReference step1() {
+      incentive.Active__c = false ;
+      incentive.Url__c = 'https://'+incentive.Name+'.invtr.co';
+        
+      // create the incentive record
+      insert incentive ;
+      return Page.InvRules;
     }
+ 
+   public PageReference step2() {
+      return Page.InvMetrics;
+   }
+ 
+   public PageReference step3() {
+      insert metrics ;
+      return save(JSON.serializePretty(incentive));
+   }
+   
+   public PageReference addRule() {
+       // store new rule
+       insert rule ;
+       IncentiveRule__c irClone = rule.clone();
+       
+       irClone.Id = rule.Id;
+       // refresh local copy
+       rules.add(irClone);
+       rule.clear();
+       return ApexPages.CurrentPage();
+   }
+   
+   public PageReference addMetric() {
+       metrics.add(metric.clone()) ;
+       metric.clear();
+       return ApexPages.CurrentPage();
+   }
     
-    public PageReference buildSite() {
+    public PageReference save(String body) {
     
         HttpRequest req = new HttpRequest();
-        System.debug('Start date:'+startDate);
-        System.debug('End date:'+endDate);
-        
-        String body = 'subdomain='+subdomain+
-			'&active=false'+
-            '&startDate='+EncodingUtil.urlEncode(startDate,'UTF-8')+
-            '&endDate='+EncodingUtil.urlEncode(endDate,'UTF-8')+
-            '&title='+EncodingUtil.urlEncode(title,'UTF-8')+
-            '&tagline='+EncodingUtil.urlEncode(tagline,'UTF-8')+
-            '&description='+EncodingUtil.urlEncode(description,'UTF-8');
             
         System.debug(body) ;
          
@@ -85,7 +222,7 @@ public class IncentiveBuilderClass {
 
         Http http = new Http();
         
-        PageReference pageRef = Page.InvSiteConfig;
+        PageReference pageRef = Page.InvAdmin;
         pageRef.setRedirect(true);
   
         try {
@@ -112,29 +249,9 @@ public class IncentiveBuilderClass {
             ApexPages.addMessages(e);
             pageRef = ApexPages.CurrentPage();
         }
-          
-        storeIncentive() ;
         
         return pageRef;
     }
     
-    public void storeIncentive() {
-    
-        try {
-            Incentive__c newIncentive = new Incentive__c() ;
-            newIncentive.Name = subdomain ;
-			newIncentive.Active__c = false ;
-            newIncentive.StartDate__c = Datetime.parse(startDate) ;
-            newIncentive.EndDate__c =Datetime.parse(endDate) ;
-            newIncentive.Title__c = title;
-            newIncentive.Tagline__c = tagline;
-            newIncentive.Description__c = description ;
-            newIncentive.Url__c = 'https://'+subdomain+'.invtr.co';
-        
-            insert newIncentive ;
-        } catch (DMLException e){
-            System.debug(e);
-        }
-
-    }
+   
 }
